dd574a25b1477a27baeb48c13e8926ba6fa4f525
Will Roden <will@roden.cc>
2023-11-22 11:00:30 -0600

    debug

--- src/set-output.sh
+++ src/set-output.sh
@@ -4,13 +4,13 @@ set -e
 
 name="$1"
 value="$2"
-#{
-#    echo "${name}<<EOF"
-#    echo "$value"
-#    echo 'EOF'
-#} >> "$GITHUB_OUTPUT"
+{
+    echo "${name}<<EOF"
+    echo "$value"
+    echo 'EOF'
+} >> "$GITHUB_OUTPUT"
 
-value="${value//'%'/'%25'}"
-value="${value//$'\n'/'%0A'}"
-value="${value//$'\r'/'%0D'}"
-echo "${name}=${value}" >> "$GITHUB_OUTPUT"
+#value="${value//'%'/'%25'}"
+#value="${value//$'\n'/'%0A'}"
+#value="${value//$'\r'/'%0D'}"
+#echo "${name}=${value}" >> "$GITHUB_OUTPUT"
