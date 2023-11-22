3bd5aab53d9400345ef77bf1d0b49b71cb430154
Will Roden <will@roden.cc>
2023-11-22 11:12:22 -0600

    restore set-output.sh

--- src/set-output.sh
+++ src/set-output.sh
@@ -9,8 +9,3 @@ value="$2"
     echo "$value"
     echo 'EOF'
 } >> "$GITHUB_OUTPUT"
-
-#value="${value//'%'/'%25'}"
-#value="${value//$'\n'/'%0A'}"
-#value="${value//$'\r'/'%0D'}"
-#echo "${name}=${value}" >> "$GITHUB_OUTPUT"
